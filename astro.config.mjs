8834fe428acd09eea6374f3a1a29e0bac0198d90
Gustavo Vázco <gustavovazco@gmail.com>
2024-12-20 12:22:36 -0600

    📦 NEW: add astro sitemap's plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,20 +4,20 @@ import { loadEnv } from "vite";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
-const { PUBLIC_WP_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+import sitemap from "@astrojs/sitemap";
+const {
+  PUBLIC_WP_URL
+} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: [PUBLIC_WP_URL],
+    domains: [PUBLIC_WP_URL]
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    robotsTxt(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react(), robotsTxt(), sitemap()],
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel()
 });
