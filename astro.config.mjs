1827915431bedbbccab9220275bf7e381c542c2c
Gustavo Vázco <gustavovazco@gmail.com>
2024-12-20 12:28:35 -0600

    🐛 FIX: add robots.txt and link's in head for sitemap's config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,22 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
-const {
-  PUBLIC_WP_URL
-} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-
+const { PUBLIC_WP_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: [PUBLIC_WP_URL]
+    domains: [PUBLIC_WP_URL],
   },
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), robotsTxt(), sitemap()],
+  site: "https://www.techosymantenimientos.com",
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    robotsTxt(),
+    sitemap(),
+  ],
   output: "hybrid",
-  adapter: vercel()
+  adapter: vercel(),
 });
